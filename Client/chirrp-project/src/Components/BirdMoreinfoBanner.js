5ab8774a51c3b9fca6c6bccead3e9f368e18d616
Gowtham MA <magowtham7@gmail.com>
2023-07-24 22:51:29 +0530

    working on bird more info banner component

--- Client/chirrp-project/src/Components/BirdMoreinfoBanner.js
+++ Client/chirrp-project/src/Components/BirdMoreinfoBanner.js
@@ -12,26 +12,76 @@ function BirdMoreinfoBanner() {
           <div className="heading-sec">
             <h1>White Bellied Sea Eagle</h1>
             <h2>Harpactes fasciatusfir</h2>
-            <h2>ಮಲಬಾರ್ ಟ್ರೋಗನ್ </h2>
+            <h2>ಮಲಬಾರ್ ಟ್ರೋಗನ್</h2>
           </div>
           <div className="biological-order-sec">
             <ul>
-              <li>1</li>
-              <div className="pipe"></div>
-              <li>2</li>
-              <div className="pipe"></div>
-              <li>3</li>
-              <div className="pipe"></div>
-              <li>4</li>
-              <div className="pipe"></div>
-              <li>5</li>
-              <div className="pipe"></div>
-              <li>6</li>
-              <div className="pipe"></div>
-              <li>7</li>
+              <li>
+                <span>1</span>
+                <hr />
+              </li>
+              <li>KINGDOM</li>
+              <li>Animalia</li>
+            </ul>
+            <ul>
+              <li>
+                <span>2</span>
+                <hr />
+              </li>
+              <li>PHYLUM</li>
+              <li>Chordata</li>
+            </ul>
+            <ul>
+              <li>
+                <span>3</span>
+                <hr />
+              </li>
+              <li>CLASS</li>
+              <li>Aves</li>
+            </ul>
+            <ul>
+              <li>
+                <span>4</span>
+                <hr />
+              </li>
+              <li>ORDER</li>
+              <li>Bucerotiformes</li>
+            </ul>
+            <ul>
+              <li>
+                <span>5</span>
+                <hr />
+              </li>
+              <li>FAMILY</li>
+              <li>Bucerotidae</li>
+            </ul>
+            <ul>
+              <li>
+                <span>6</span>
+                <hr />
+              </li>
+              <li>GENUS</li>
+              <li>Ocyceros</li>
+            </ul>
+            <ul>
+              <li>
+                <span>7</span>
+              </li>
+              <li>SPECIES</li>
+              <li>Ocyceros griseus</li>
             </ul>
           </div>
         </div>
+      </div>
+      <div className="test">
+        <ul>
+          <li>
+            <span>1</span>
+            <hr />
+          </li>
+          <li>KINGDOM</li>
+          <li>Animalia hello world hello world hello world</li>
+        </ul>
       </div>
     </>
   );
